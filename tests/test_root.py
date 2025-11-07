20377799732c04bd8d279433158ab751f2824dff
Rajdev Tiwari <drrajdevtiwari@gmail.com>
2025-11-07 11:30:04 +0530

    Updated FastAPI Hello World

--- tests/test_root.py
+++ tests/test_root.py
@@ -1,10 +1,11 @@
-from httpx import AsyncClient
 import pytest
+import httpx
 from app.main import app
 
 @pytest.mark.asyncio
 async def test_root():
-    async with AsyncClient(app=app, base_url="http://test") as ac:
-        r = await ac.get("/")
-    assert r.status_code == 200
-    assert r.json() == {"message": "Hello from FastAPI#2!"}
+    transport = httpx.ASGITransport(app=app)
+    async with httpx.AsyncClient(transport=transport, base_url="http://test") as ac:
+        response = await ac.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello from FastAPI!"}
