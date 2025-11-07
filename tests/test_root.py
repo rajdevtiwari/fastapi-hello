4a91154a28d749bff31bb455bfa94a276b5c88dc
Rajdev Tiwari <drrajdevtiwari@gmail.com>
2025-11-07 10:16:44 +0530

    Updated FastAPI Hello World

---
+++ tests/test_root.py
@@ -0,0 +1,10 @@
+from httpx import AsyncClient
+import pytest
+from main import app
+
+@pytest.mark.asyncio
+async def test_root():
+    async with AsyncClient(app=app, base_url="http://test") as ac:
+        r = await ac.get("/")
+    assert r.status_code == 200
+    assert r.json() == {"message": "Hello from FastAPI!"}
