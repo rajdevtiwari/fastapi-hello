dc4f0ea6726fc0a7c82dc42bc2f07e62991717ae
Rajdev Tiwari <drrajdevtiwari@gmail.com>
2025-11-07 13:20:21 +0530

    Updated FastAPI Hello World

--- tests/test_root.py
+++ tests/test_root.py
@@ -8,4 +8,4 @@ async def test_root():
     async with httpx.AsyncClient(transport=transport, base_url="http://test") as ac:
         response = await ac.get("/")
     assert response.status_code == 200
-    assert response.json() == {"message": "Hello World from FastAPI#4!"}
+    assert response.json() == {"message": "Hello World from FastAPI Version#4!"}
